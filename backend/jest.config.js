1a999b66f8177a02c112ae031e8f458a4d770e00
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2023-05-25 08:37:49 -0300

    :art: build: adicionando arquivos de configuração

---
+++ backend/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globalSetup": "<rootDir>/__tests__/_globalSetup.js",
+  "testSequencer": "<rootDir>/__tests__/_testSequencer.js",
+  "testMatch": [
+    "**/__tests__/*.test.js"
+  ],
+  "testTimeout": 15000
+}
